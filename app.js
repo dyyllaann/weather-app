d308907a1e119b830a6ad2b3008f21299aea6fbe
dyyllaann <dylan.arveson@gmail.com>
2022-02-22 19:41:38 -0800

    Parse returned data object.

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const city = "London";
+let city = "London";
   
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=017526151d00c5ca477e7e5f228ce14b`)
   .then(function (response) {
-    console.log(response.json());
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(data.name);
   })
   .catch(function (err) {
-    console.log("It didn't work!");
+    console.log('Error:', err);
   });
