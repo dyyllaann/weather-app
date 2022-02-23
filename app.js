604edff75424952a62383bcff35ada0d19fb48eb
dyyllaann <dylan.arveson@gmail.com>
2022-02-22 18:49:55 -0800

    Fetch data by using city name variable.

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-
-// URL (required), options (optional)
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=017526151d00c5ca477e7e5f228ce14b')
-  // mode: 'cors'
-  .then(function(response) {
+const city = "London";
+  
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=017526151d00c5ca477e7e5f228ce14b`)
+  .then(function (response) {
     console.log(response.json());
   })
-  .catch(function(err) {
-    // Error :(
+  .catch(function (err) {
     console.log("It didn't work!");
   });
